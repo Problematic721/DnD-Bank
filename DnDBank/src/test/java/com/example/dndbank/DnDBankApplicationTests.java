181991c57d295d9793a46b5d2cab8a08d4e6ad4a
Alex <alexyao12345@gmail.com>
2024-09-23 09:47:18 +0800

    Changed Project name

--- DnDBank/src/test/java/com/example/dndbank/DnDBankApplicationTests.java
+++ DnDBank/src/test/java/com/example/dndbank/DnDBankApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DnDWallet1ApplicationTests {
+class DnDBankApplicationTests {
 
 	@Test
 	void contextLoads() {
